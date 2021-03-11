eddd6b90a18814fa19c297b37dce267fcaa36099
Samuel854 <71836919+Samuel854@users.noreply.github.com>
2021-03-11 21:28:01 +0100

    Seperated Methods- added Test cases

---
+++ src/main/java/CheckPassword.java
@@ -0,0 +1,154 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class CheckPassword {
+
+    private static final String PASSWORD_PATTERN = "^(?=.*[0-9])" + //Atleast one Digit.
+            "(?!.*(012|123|234|345|456|567|678|789|890))" + // No use of rows of numbers.
+            "(?!.*(1111|2222|3333|4444|5555|6666|7777|8888|9999|0000))" + // No use of same numbers in pairs of four.
+            "(?=.*[a-z])" + //Atleast one lower case.
+            "(?=.*[A-Z])" + //Atleast one upper case.
+            "(?=.*[()#$?!%/@])" + //Atleast one of the following special characters.
+            "(?!.*[&–[{}]:;',*~^+=<>])" + //None of these following characters should be used.
+            ".{8,25}$"; //Length of 8-25 characters.
+
+
+    private static final String PASSWORD_PATTERN_DIGIT = "(?=.*[0-9])";
+    private static final String PASSWORD_PATTERN_ORDER = "(?!.*(012|123|234|345|456|567|678|789|890))";
+    private static final String PASSWORD_PATTERN_ROW = "(?!.*(1111|2222|3333|4444|5555|6666|7777|8888|9999|0000))";
+    private static final String PASSWORD_PATTERN_LOWER = "(?=.*[a-z])";
+    private static final String PASSWORD_PATTERN_UPPER = "(?=.*[A-Z])";
+    private static final String PASSWORD_PATTERN_SPECIAL = "(?=.*[()#$?!%/@])";
+    private static final String PASSWORD_PATTERN_NOSPECIAL = "(?!.*[&–[{}]:;',*~^+=<>])";
+    private static final String PASSWORD_PATTERN_LENGTH = ".{8,25}$";
+
+
+    public static boolean validPassword_Length(String password) {
+
+        if (password.matches(PASSWORD_PATTERN_LENGTH)) {
+            System.out.println("Valid");
+            return true;
+        } else {
+            System.out.println("Password not valid. Try Again!");
+            return false;
+        }
+
+    }
+
+    public static boolean validPassword_Digit(String password) {
+
+        if (password.matches(PASSWORD_PATTERN_DIGIT)) {
+            System.out.println("Valid");
+            return true;
+        } else {
+            System.out.println("Password not valid. Try Again!");
+            return false; // returns false
+        }
+
+
+    }
+
+    public static boolean validPassword_Order(String password) {
+
+        if (password.matches(PASSWORD_PATTERN_ORDER)) {
+            System.out.println("Valid");
+            return true;
+        } else {
+            System.out.println("Password not valid. Try Again!");
+            return false;
+        }
+
+    }
+
+    public static boolean validPassword_Row(String password) {
+
+        if (password.matches(PASSWORD_PATTERN_ROW)) {
+            System.out.println("Valid");
+            return true;
+        } else {
+            System.out.println("Password not valid. Try Again!");
+            return false;
+        }
+
+    }
+
+    public static boolean validPassword_Lower(String password) {
+
+        if (password.matches(PASSWORD_PATTERN_LOWER)) {
+            System.out.println("Valid");
+            return true;
+        } else {
+            System.out.println("Password not valid. Try Again!");
+            return false;
+        }
+
+    }
+
+    public static boolean validPassword_Upper(String password) {
+
+        if (password.matches(PASSWORD_PATTERN_UPPER)) {
+            System.out.println("Valid");
+            return true;
+        } else {
+            System.out.println("Password not valid. Try Again!");
+            return false;
+        }
+
+    }
+
+    public static boolean validPassword_Special(String password) {
+
+        if (password.matches(PASSWORD_PATTERN_SPECIAL)) {
+            System.out.println("Valid");
+            return true;
+        } else {
+            System.out.println("Password not valid. Try Again!");
+            return false;
+        }
+
+    }
+
+    public static boolean validPassword_No_Special(String password) {
+
+        if (password.matches(PASSWORD_PATTERN_NOSPECIAL)) {
+            System.out.println("Valid");
+            return true;
+        } else {
+            System.out.println("Password not valid. Try Again!");
+            return false;
+        }
+
+
+    }
+
+
+
+
+    public static void main(String[] args){
+
+        System.out.println("CHECK PASSWORD! \n");
+        System.out.println("* 8 - 25 characters long.");
+        System.out.println("* Upper and lowercase.");
+        System.out.println("* Atleast one digits/number.");
+        System.out.println("* Atleast one special character. ()#$?!%/@ Other characters are not allowed.");
+        System.out.println("* No order of numbers over two numbers. (12,23,34 etc.)");
+        System.out.println("* Only use the same number 3 times in a row. 4 not allowed. (111, 222, 333, 444 etc.)\n");
+        System.out.println("Type Password:");
+
+            /*if (password.length() < 8 || password.length() > 25){
+            return Boolean.FALSE;
+        }
+        return Boolean.TRUE;*/
+
+        //Scanner scanner = new Scanner(System.in); //use for main.
+
+        //password = scanner.next(); //use for main.
+
+
+
+
+    }
+
+
+}
+
---
+++ src/test/java/CheckPasswordTest.java
@@ -0,0 +1,161 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class CheckPasswordTest {
+
+    @BeforeAll
+    static void init(){}
+
+    @BeforeEach
+    void setUp(){}
+
+    @Test
+    void validPassword1_Length(){ //Length should be >= 8.
+        CheckPassword checkPassword = new CheckPassword();
+        boolean actual = checkPassword.validPassword_Length("Helloworld");
+        assertTrue(actual);
+
+    }
+
+    @Test
+    void validPassword1_Too_Long(){ //Length over 25 characters.
+        CheckPassword checkPassword = new CheckPassword();
+        boolean actual = checkPassword.validPassword_Length("HelloworldBlablablbalbalablabala");
+        assertFalse(actual);
+
+    }
+
+    @Test
+    void validPassword1_Not_Too_Long(){ //Password is not too long/ > 25.
+        CheckPassword checkPassword = new CheckPassword();
+        boolean actual = checkPassword.validPassword_Length("Helloworld");
+        assertTrue(actual);
+
+    }
+
+    @Test
+    void validPassword1_Not_Too_Short(){ //Password is too short/ < 8.
+        CheckPassword checkPassword = new CheckPassword();
+        boolean actual = checkPassword.validPassword_Length("Helloworld124#");
+        assertTrue(actual);
+
+    }
+
+    @Test
+    void validPassword1_Too_Short(){ //Password is too short/ < 8.
+        CheckPassword checkPassword = new CheckPassword();
+        boolean actual = checkPassword.validPassword_Length("Hello1#");
+        assertFalse(actual);
+
+    }
+
+    @Test
+    void validPassword1_Number_Digit(){ //Password needs at least one Digit/Number.
+        CheckPassword checkPassword = new CheckPassword();
+        boolean actual = checkPassword.validPassword_Digit("Helloworld124#");
+        assertTrue(actual);
+
+    }
+
+    @Test
+    void validPassword1_No_Number_Digit(){ //Password needs at least one Digit/Number.
+        CheckPassword checkPassword = new CheckPassword();
+        boolean actual = checkPassword.validPassword_Digit("Helloworld#");
+        assertFalse(actual);
+
+    }
+
+    @Test
+    void validPassword3_Uppercase() { //Uppercase?
+        CheckPassword checkPassword = new CheckPassword();
+        boolean actual = checkPassword.validPassword_Upper("Helloworld124#");
+        assertTrue(actual);
+    }
+
+    @Test
+    void validPassword3_No_Uppercase() { //Uppercase?
+        CheckPassword checkPassword = new CheckPassword();
+        boolean actual = checkPassword.validPassword_Upper("helloworld124#");
+        assertFalse(actual);
+    }
+
+    @Test
+    void validPassword5_LowerCase() { //Lowercase?
+        CheckPassword checkPassword = new CheckPassword();
+        boolean actual = checkPassword.validPassword_Lower("helloworld");
+        assertTrue(actual);
+    }
+
+    @Test
+    void validPassword5_No_LowerCase() { //Lowercase?
+        CheckPassword checkPassword = new CheckPassword();
+        boolean actual = checkPassword.validPassword_Lower("HELLOWORLD124#");
+        assertFalse(actual);
+    }
+
+    @Test
+    void validPassword6_Special_Characters() { //Use of given special characters.
+        CheckPassword checkPassword = new CheckPassword();
+        boolean actual = checkPassword.validPassword_Special("Helloworld124#");
+        assertTrue(actual);
+    }
+
+    @Test
+    void validPassword6_No_Special_Characters() { //Use of given special characters.
+        CheckPassword checkPassword = new CheckPassword();
+        boolean actual = checkPassword.validPassword_Special("Helloworld124");
+        assertFalse(actual);
+    }
+
+    @Test
+    void validPassword6_Forbidden_Special_Characters() { //Use of given special characters.
+        CheckPassword checkPassword = new CheckPassword();
+        boolean actual = checkPassword.validPassword_No_Special("Helloworld124&");
+        assertFalse(actual);
+    }
+
+    @Test
+    void validPassword6_No_Number_Order() { //Use of given special characters.
+        CheckPassword checkPassword = new CheckPassword();
+        boolean actual = checkPassword.validPassword_Order("Helloworld124#");
+        assertTrue(actual);
+    }
+
+    @Test
+    void validPassword6_Number_Order() { //Use of given special characters.
+        CheckPassword checkPassword = new CheckPassword();
+        boolean actual = checkPassword.validPassword_Order("Helloworld456#");
+        assertFalse(actual);
+    }
+
+    @Test
+    void validPassword6_Number_Row() { //Use of given special characters.
+        CheckPassword checkPassword = new CheckPassword();
+        boolean actual = checkPassword.validPassword_Row("Helloworld4444#");
+        assertFalse(actual);
+    }
+
+    @Test
+    void validPassword6_No_Number_Row() { //Use of given special characters.
+        CheckPassword checkPassword = new CheckPassword();
+        boolean actual = checkPassword.validPassword_Row("Helloworld444#");
+        assertTrue(actual);
+    }
+
+
+
+
+// https://users.csc.calpoly.edu/~djanzen/courses/405W13/presentations/tdd.pdf
+// https://mkyong.com/regular-expressions/how-to-validate-password-with-regular-expression/
+// https://www.agilealliance.org/glossary/tdd#q=~(infinite~false~filters~(postType~(~'page~'post~'aa_book~'aa_event_session~'aa_experience_report~'aa_glossary~'aa_research_paper~'aa_video)~tags~(~'tdd))~searchTerm~'~sort~false~sortDirection~'asc~page~1)
+// http://www.luv2code.com/category/test-driven-development/page/2/
+// https://www.geeksforgeeks.org/program-to-check-the-validity-of-a-password/
+// https://stackoverflow.com/questions/42967934/java-regex-to-not-allow-string-special-characters
+// https://stackoverflow.com/questions/17715388/regex-java-matching-strings-012123234345456567678789890
+
+
+}
